e7d288eb61aae6b485536b409d2503ce95cac057
Quan <toolowner@hotmail.com>
2020-08-06 11:47:32 +0700

    snap selection to word before annotating

--- src/annotator/selections.js
+++ src/annotator/selections.js
@@ -2,6 +2,7 @@ import * as observable from './util/observable';
 
 /** Returns the selected `DOMRange` in `document`. */
 function selectedRange(document) {
+  snapSelectionToWord(document);
   const selection = document.getSelection();
   if (!selection.rangeCount || selection.getRangeAt(0).collapsed) {
     return null;
@@ -53,4 +54,50 @@ export default function selections(document) {
   return events.map(function () {
     return selectedRange(document);
   });
+}
+
+//Credit: https://stackoverflow.com/a/10964743/5827002
+function snapSelectionToWord(document) {
+  let sel;
+
+  if (document.getSelection && (sel = document.getSelection()).modify) {
+    sel = document.getSelection();
+    if (!sel.isCollapsed) {
+      // Detect if selection is backwards
+      let range = document.createRange();
+      range.setStart(sel.anchorNode, sel.anchorOffset);
+      range.setEnd(sel.focusNode, sel.focusOffset);
+      let backwards = range.collapsed;
+      range.detach();
+
+      // modify() works on the focus of the selection
+      let endNode = sel.focusNode,
+        endOffset = sel.focusOffset;
+      sel.collapse(sel.anchorNode, sel.anchorOffset);
+      if (backwards) {
+        sel.modify('move', 'backward', 'character');
+        sel.modify('move', 'forward', 'word');
+        sel.extend(endNode, endOffset);
+        sel.modify('extend', 'forward', 'character');
+        sel.modify('extend', 'backward', 'word');
+      } else {
+        sel.modify('move', 'forward', 'character');
+        sel.modify('move', 'backward', 'word');
+        sel.extend(endNode, endOffset);
+        sel.modify('extend', 'backward', 'character');
+        sel.modify('extend', 'forward', 'word');
+      }
+    }
+  } else if ((sel = document.selection) && sel.type !== 'Control') {
+    let textRange = sel.createRange();
+    if (textRange.text) {
+      textRange.expand('word');
+      // Move the end back to not include the word's trailing space(s),
+      // if necessary
+      while (/\s$/.test(textRange.text)) {
+        textRange.moveEnd('character', -1);
+      }
+      textRange.select();
+    }
+  }
 }
